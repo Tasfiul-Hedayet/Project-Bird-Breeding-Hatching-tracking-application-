059ae9cf93484aa467b7b5f755156f8b7d98228a
Tasfiul Hedayet <63163295+Tasfiul-Hedayet@users.noreply.github.com>
2024-02-09 00:07:07 +0600

    update

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,52 +1,28 @@
-// Hatch Tracker:
-//
-// ·        Hatch Number (should increment)
-// ·        Identification (option to increment or input, check for duplicates)
-// ·        Species
-// ·        Breed
-// ·        Cock Identifer (cross reference to Bird Tracker Database)
-// ·        Hen Identifer (cross reference to Bird Tracker Database)
-// ·        Set (Date)
-// ·        Lockdown (date)
-// ·        Hatch (date)
-// ·        # Set
-// ·        #Fertile
-// ·        # Hatch
-
+import Navbar from "@/components/Navbar";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <header>
-        <div class="logo">Knudson Farm</div>
-        <div class="hamburger">
-          <div class="line"></div>
-          <div class="line"></div>
-          <div class="line"></div>
-          <div class="line"></div>
-        </div>
-        <nav class="nav-bar">
-          <ul>
-            <li>
-              <Link href="" class="active">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link href="/bird">Bird</Link>
-            </li>
-            <li>
-              <Link href="/hatch">Hatch</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <div className="hero-section">
-        <div>
-          <h1>Bird Breeding & Hatching tracking application</h1>
-          <button> <Link href="/bird">Add Bird</Link></button>
-          <button><Link href="/hatch">Add Hatch</Link></button>
+      <div className="body">
+        <Navbar />
+        <div className="hero-section">
+          <div className="container">
+            <h1 className="heading">
+              Bird <span className="text-orange-400">Breeding</span> &{" "}
+            </h1>
+            <h1 className="heading-2">Hatching tracking</h1>
+            <h1 className="heading-3">application</h1>
+            <button className="button-1">
+              <Link href="/bird">Add Bird</Link>
+            </button>
+            <button className="button-2">
+              <Link href="/hatch">Add Hatch</Link>
+            </button>
+          </div>
+
+          <div className="bg-cover"></div>
         </div>
       </div>
     </>
---
+++ frontend/components/Navbar.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Link from 'next/link'
+
+function Navbar() {
+  return (
+    <div>
+      <header>
+        <div className="logo">Knudson Farm</div>
+        <div className="hamburger">
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+        </div>
+        <nav className="nav-bar">
+          <ul>
+            <li>
+              <Link href="" className="active">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/bird">Bird</Link>
+            </li>
+            <li>
+              <Link href="/hatch">Hatch</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default Navbar;
